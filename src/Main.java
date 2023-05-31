c6585369b6ee2559228a6d70dfb2387a7ab3375e
cildemimec <cildemimec@hotmail.com>
2023-05-31 16:37:35 +0300

    etkinlik-onerme
    kodluyoruz odev java ile etkinlik onerme

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sıcaklık giriniz : ");
+        heat = input.nextInt();
+
+        if (heat < 5){
+            System.out.println("Kayak yapabilirsiniz");
+        }else if (heat <= 25 ){
+            if (heat <= 15){
+                System.out.println("Sinemaya gidebilirsiniz");
+            }
+            if(heat >= 10 ){
+                System.out.println("Pikniğe gidebilirsin");
+            }
+        } else {
+            System.out.println("yüzmeye gidebilirsiniz.");
+        }
+
+    }
+}
